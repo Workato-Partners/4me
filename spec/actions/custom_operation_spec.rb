156b9fce3fe1dc0e084c53cfde04bbf2b22c8708
Klaas <klaas.vandeweerdt@4me.com>
2023-03-08 10:45:43 +0100

    Added additional specs for custom operation with operation name

--- spec/actions/custom_operation_spec.rb
+++ spec/actions/custom_operation_spec.rb
@@ -1,16 +1,16 @@
 # frozen_string_literal: true
 
-RSpec.describe 'actions/custom_operation', :vcr do
+RSpec.describe 'actions/custom_operation', :vcr do # rubocop:disable Metrics/BlockLength
   # Spec describes the most commons blocks of an action. Remove describes that you don't need.
   # Learn more: https://docs.workato.com/developing-connectors/sdk/cli/reference/rspec-commands.html
 
   let(:connector) { Workato::Connector::Sdk::Connector.from_file('connector.rb', settings) }
   let(:settings) { Workato::Connector::Sdk::Settings.from_encrypted_file('settings.yaml.enc', 'master.key') }
-  let(:input) { JSON.parse(File.read('fixtures/actions/custom_operation/input/query.json')) }
 
-  subject(:output) { connector.actions.custom_operation(input) }
+  describe 'test custom operation with single query' do
+    let(:input) { JSON.parse(File.read('fixtures/actions/custom_operation/input/query.json')) }
+    subject(:output) { connector.actions.custom_operation(input) }
 
-  describe 'test custom operation' do
     it 'contains a response' do
       expect(output).to be_present
     end
@@ -22,6 +22,29 @@ RSpec.describe 'actions/custom_operation', :vcr do
 
     it 'contains id account and configurationItems' do
       expect(output[:people][:nodes].first).to include('id', 'configurationItems', 'account')
+    end
+  end
+
+  describe 'test custom operation with multiple queries' do
+    let(:input) { JSON.parse(File.read('fixtures/actions/custom_operation/input/query_operation_name.json')) }
+    subject(:output) { connector.actions.custom_operation(input) }
+
+    it 'contains a response' do
+      expect(output).to be_present
+    end
+
+    it 'contains id, name and primary email' do
+      expect(output[:node]).to include('id', 'name', 'primaryEmail')
+      expect(output[:node][:primaryEmail]).to eq('aleksei.zelensk@microsoft.com')
+    end
+  end
+
+  describe 'test custom operation with multiple queries but no operation selected' do
+    let(:input) { JSON.parse(File.read('fixtures/actions/custom_operation/input/query_operation_name_error.json')) }
+    subject(:output) { connector.actions.custom_operation(input) }
+
+    it 'contains an MissingRequiredInput error' do
+      expect { output }.to raise_error(Workato::Connector::Sdk::MissingRequiredInput)
     end
   end
 end
